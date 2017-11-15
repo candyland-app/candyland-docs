17242b5ed1eb7d9089cdd231f98373806ba6223f
Klaus Sinani <klauscfhq@protonmail.com>
2017-11-15 22:22:03 +0200

    Styling & final tweaks

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 'use strict';
 const fs = require('fs');
+const path = require('path');
 const chalk = require('chalk');
 const rimraf = require('rimraf');
 const markdownPDF = require('markdown-pdf');
+
+const join = path.join;
+const resolve = path.resolve;
 
 const green = chalk.bold.green;	// Green bold text
 const yellow = chalk.bold.yellow;	// Yellow bold text
 
 const distPath = 'dist/';	// Dist directory
+const stylePath = resolve(__dirname, 'style');	// Style directory
+const cssFile = join(stylePath, 'index.css');	// Main styling file
+
 const mdDocs = ['reports/requirements-analysis.md'];	// Markdown source files
 
 const pdfOptions = {
 	// PDF conversion options
+	cssPath: cssFile,
 	paperFormat: 'A4',
 	paperBorder: '2cm',
-	paperOrientation: 'portrait'
+	paperOrientation: 'portrait',
+	remarkable: {
+		html: true,
+		breaks: true
+	}
 };
 
 const pdfDocs = mdDocs.map(mdDoc => {
