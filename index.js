c85c016b1b79fe0d17ca98cec0d1872109a1de93
Klaus Sinani <klauscfhq@protonmail.com>
2018-01-11 01:59:02 +0200

    Automatically build PDFs on file change

--- index.js
+++ index.js
@@ -1,64 +1,37 @@
+#!/usr/bin/env node
 'use strict';
-const fs = require('fs');
 const path = require('path');
-const mdpdf = require('mdpdf');
 const chalk = require('chalk');
 const dir = require('node-dir');
-const rimraf = require('rimraf');
-const kumaPath = require('kumacss');
+const watch = require('node-watch');
+const buildPDF = require('./bin/pdf.js');
 
 const resolve = path.resolve;
 
+const red = chalk.bold.red;	// Red bold text
 const green = chalk.bold.green;	// Green bold text
 const yellow = chalk.bold.yellow;	// Yellow bold text
+const magenta = chalk.bold.magenta;	// Magenta bold text
 
-const cssFile = kumaPath();	// Main styling file
-const distPath = resolve(__dirname, 'dist');	// Dist directory
 const reportsPath = resolve(__dirname, 'reports');	// Reports directory
 
 // Get all markdown docs
 const mdDocs = dir.files(reportsPath, {sync: true, recursive: false});
 
-function getPdfPath(mdDocPath) {
-	// Get the path of the PDF file to be created
-	const pdfDoc = mdDocPath.replace('reports', 'dist').replace('.md', '.pdf');
-	return pdfDoc;
-}
+console.log(green('Watching for File Changes... @ ') + yellow(reportsPath));
 
-function buildPDF(mdDocPath) {
-	// Build PDF from input markdown file
-	const pdfDocPath = getPdfPath(mdDocPath);
-	// Building options
-	const options = {
-		source: mdDocPath,
-		destination: pdfDocPath,
-		styles: cssFile,
-		pdf: {
-			format: 'A4'
-		}
-	};
-	// Initialize building process
-	mdpdf.convert(options).then(pdfPath => {
-		console.log();
-		console.log(green('✔ Created PDF file: ') + yellow(pdfPath));
-	}).catch(err => {
-		console.error(err);
-	});
-	return 0;
-}
-
-if (fs.existsSync(distPath)) {
-	// Check if `dist` directory already exist
-	try {
-		// Cleanup
-		rimraf.sync(distPath);
-		console.log(green('✔ Cleaning up'));
-	} catch (err) {
-		console.error(err);
+// Start watching for changes
+watch(mdDocs, (evt, doc) => {
+	// Build PDF when on doc update
+	if (evt === 'update') {
+		console.log(green('File was Updated: ') + yellow(doc));
+		// Build PDF
+		buildPDF(doc);
 	}
-}
-
-for (let i = 0; i < mdDocs.length; i++) {
-	// Run the building for all markdown files
-	buildPDF(mdDocs[i]);
-}
+	// Exit on doc deletion
+	if (evt === 'remove') {
+		// A doc was deleted
+		console.log(red('File was Deleted: ') + magenta(doc));
+		process.exit(1);
+	}
+});
